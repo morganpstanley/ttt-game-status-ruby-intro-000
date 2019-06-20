925a929da78823cf46803df0d3f368cef355e9af
Morgan Stanley <morganpstanley@gmail.com>
2019-06-20 02:36:00 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,64 +5,39 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8]
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
 ]
 
+# Define won?, full?, draw?, over?, and winner below
 def won?(board)
-WIN_COMBINATIONS.each {|win_combination|
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
-
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
-
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination
-  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-    return win_combination
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
   end
-}
-  return false
 end
 
 def full?(board)
-  if board.any? { |i| i == " " }
-    return false
-  else
-    return true
-  end
+  board.all?{|token| token == "X" || token == "O"}
 end
 
 def draw?(board)
-  if full?(board) == true && won?(board) == false
-    return true
-  else
-    return false
-  end
+  full?(board) && !won?(board)
 end
 
 def over?(board)
-  if won?(board) != false || full?(board) == true || draw?(board) == true
-    return true
-  else
-    return false
-  end
+  won?(board) || full?(board)
 end
 
 def winner(board)
-  i = won?(board)
-  if won?(board) != false
-     return won?(board[0])
-   else
-     return nil
-   end
- end
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
+end
